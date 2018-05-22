8d84ed15376969f024bf678dc63d57b73ffb3ccd
Davi Cesar Correia Jr <davi@qulture.rocks>
2018-05-22 17:22:38 -0300

    Add check if dividor is greater than dividend

--- src/lab3/division.s
+++ src/lab3/division.s
@@ -2,9 +2,11 @@
   .globl main
 
 main:
-  MOVS r1, #27  /* r1 = dividend */
+  MOVS r1, #3  /* r1 = dividend */
   MOVS r2, #5 /* r2= divisor */
   MOVS r3, #0 /* r3 = quocient */
+  CMP r1, r2
+  BCC end
   BL alignDivisorLeft
 compare:
   CMP r5, #0
