f2de0470b44d289e9ffeeb090a7d384764b85078
Davi Cesar Correia Jr <davi@qulture.rocks>
2018-06-12 16:47:50 -0300

    Fix state machine

--- src/lab5/5.5.4.2_Finite_State_Machines.s
+++ src/lab5/5.5.4.2_Finite_State_Machines.s
@@ -2,25 +2,24 @@
   .globl main
 
 main:
-  LDR r8, =0b0110110 /* X */
+  MOV r0, #1
+  LDR r8, =0xaaaa5555 /* X */
   LDR r2, =0 /* Z */
-  LDR r1, =0b1111 /* Máscara */
+  LDR r1, =0b111 /* Máscara */
   LDR r3, =0 /* Counter */
-  LDR r4, =0b1011 /* Gabarito */
-  LDR r9, =0x7 /* MAX */
-  SUB r9, r9, #4
+  LDR r4, =5 /* Gabarito */
+  LDR r9, =33 /* MAX */
+  SUB r9, r9, #3
   CMP r9, r3
   BMI end
 loop:
   AND r6, r1, r8
   CMP r6, r4
-  ADDEQ r2, r2, #1
+  ADDEQ r2, r2, r0, LSL r3
   ADD r3, r3, #1
   CMP r3, r9
-  BEQ end
-  MOV r2, r2, LSL #1
-  MOV r1, r1, LSL #1
-  MOV r4, r4, LSL #1
+  BHI end
+  MOV r8, r8, LSR #1
   BLT loop
 end:
   MOV r6, r7
