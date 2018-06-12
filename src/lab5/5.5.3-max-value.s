0210b9dd236b360de7ce07e1782fec77e518dca1
Pedro Darvas <darvas.appsimples@gmail.com>
2018-06-12 16:02:44 -0300

    Ex3.3

--- src/lab5/5.5.3-max-value.s
+++ src/lab5/5.5.3-max-value.s
@@ -3,39 +3,39 @@
 
 main:
   MOVS r0, #31
-  LDR r1, =0x5006
+  LDR r1, =0x5004
   STR r0, [r1]
 
   MOVS r0, #512
-  LDR r1, =0x500A
+  LDR r1, =0x5008
   STR r0, [r1]
 
   LDR r0, =3145
-  LDR r1, =0x500E
+  LDR r1, =0x500C
   STR r0, [r1]
 
   LDR r0, =1
-  LDR r1, =0x5012
+  LDR r1, =0x5010
   STR r0, [r1]
 
   LDR r0, =6431235
-  LDR r1, =0x5016
+  LDR r1, =0x5014
   STR r0, [r1]
 
   LDR r0, =231
-  LDR r1, =0x501A
+  LDR r1, =0x5018
   STR r0, [r1]
 
   LDR r0, =31234
-  LDR r1, =0x501E
+  LDR r1, =0x501C
   STR r0, [r1]
 
   LDR r0, =312
-  LDR r1, =0x5022
+  LDR r1, =0x5020
   STR r0, [r1]
 
   LDR r0, =31123
-  LDR r1, =0x5026
+  LDR r1, =0x5024
   STR r0, [r1]
 
 
@@ -44,7 +44,7 @@ main:
   LDR r5, =9                 @ r5 = 9
   LDR r0, =0                 @ r0 = 0
   LDR r1, =0x5000            @ r1 = 0x5000
-  LDR r2, =0x5006            @ r2 = 0x5006
+  LDR r2, =0x5004            @ r2 = 0x5006
   LDR r3, =0                 @ r3 = 0
   
 loop:
