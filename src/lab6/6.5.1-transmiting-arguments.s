446f48675d914ccab1e29bc484407221569eff64
Davi Cesar Correia Jr <davi@qulture.rocks>
2018-06-19 15:34:13 -0300

    finish second part of 6.5.1

--- src/lab6/6.5.1-transmiting-arguments.s
+++ src/lab6/6.5.1-transmiting-arguments.s
@@ -3,12 +3,24 @@
   dados: .space 100
 
 main:
+  B by_address
 by_register:
   MOV r0, #2      @ b = 2
   MOV r1, #3      @ c = 3
   MOV r2, #5      @ d = 5
   BL function_by_register
+by_address:
+  MOV r0, #2                  @ b = 2
+  MOV r1, #3                  @ c = 3
+  MOV r2, #5                  @ d = 5
+  MOV r3, #0x8000             @ r3 = 0x8000 (base address)
+  STMIA r3, {r0, r1, r2}      @ store values
+  BL function_by_address
   SWI 0x0
 
 function_by_register:
   MLA r3, r0, r1, r2
+
+function_by_address:
+  LDMIA r3, {r4, r5, r6}
+  MLA r7, r0, r1, r2
