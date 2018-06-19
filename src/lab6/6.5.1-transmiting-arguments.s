4ddd2cf49c505e8734d0f54395afc3df85a9ff95
Davi Cesar Correia Jr <davi@qulture.rocks>
2018-06-19 16:08:44 -0300

    Finish third part of 6.5.1

--- src/lab6/6.5.1-transmiting-arguments.s
+++ src/lab6/6.5.1-transmiting-arguments.s
@@ -3,12 +3,14 @@
   dados: .space 100
 
 main:
-  B by_address
+  B by_stack
+
 by_register:
   MOV r0, #2      @ b = 2
   MOV r1, #3      @ c = 3
   MOV r2, #5      @ d = 5
   BL function_by_register
+
 by_address:
   MOV r0, #2                  @ b = 2
   MOV r1, #3                  @ c = 3
@@ -16,7 +18,14 @@ by_address:
   MOV r3, #0x8000             @ r3 = 0x8000 (base address)
   STMIA r3, {r0, r1, r2}      @ store values
   BL function_by_address
-  SWI 0x0
+
+by_stack:
+  MOV r0, #2                  @ b = 2
+  MOV r1, #3                  @ c = 3
+  MOV r2, #5                  @ d = 5
+  STMFA r13!, {r0, r1, r2}    @ values into the stack
+  BL function_by_stack_1
+  MOV r0, r7
 
 function_by_register:
   MLA r3, r0, r1, r2
@@ -24,3 +33,18 @@ function_by_register:
 function_by_address:
   LDMIA r3, {r4, r5, r6}
   MLA r7, r0, r1, r2
+
+function_by_stack_1:
+  LDMFA r13!, {r4, r5, r6}
+  MUL r5, r4, r5
+  STMFA r13!, {r5, r6, lr}
+  BL function_by_stack_2
+  LDMFA r13!, {lr}
+  MOV pc, lr
+
+function_by_stack_2:
+  LDMFA r13!, {r7, r8, r9}
+  STMFA r13!, {r9}
+  ADD r7, r7, r8
+  MOV pc, lr
+ 
