1b019f69f398acbc878eb6085fca546c81d9c4b0
Unknown <guilhermeadissy@gmail.com>
2018-06-19 16:10:55 -0300

    Fixing errors on compile

--- src/lab6/6.5.3-magic-squares.s
+++ src/lab6/6.5.3-magic-squares.s
@@ -2,24 +2,25 @@
   .globl main
 
 main: 
-  LDR r0, =dados 			/* ponteiro para dados */
-  LDR r1, =4 				/* N */
-  MLA r2, r1, r1, #1 		/* r2 = N^2 + 1 */
-  MUL r2, r2, r1 			/* r2 = N(N^2 + 1) */
+  LDR r0, =dados 			  /* ponteiro para dados */
+  LDR r1, =4 				    /* N */
+  MUL r2, r1, r1		    /* r2 = N^2 */
+  ADD r2, r2, #1        /* r2 = N^2 + 1 */
+  MUL r2, r2, r1 			  /* r2 = N(N^2 + 1) */
   MOV r2, r2, LSR #1		/* r2 = N(N^2 + 1)/2 */
 
 row:
   MOV r3, r0
   MOV r10, #0
 rowLoop:
-  CPM r10, r1
+  CMP r10, r1
   BGE column
-  LDM r3!, {r5-r8}
+  LDMIA r3!, {r5-r8}
   ADD r4, r5, r6
   ADD r4, r4, r7
   ADD r4, r4, r8
   ADD r10, r10, #1
-  CPM r4, r2
+  CMP r4, r2
   BEQ rowLoop
   B final
 
@@ -39,6 +40,5 @@ final:
 
 
 
-
-data: .byte #16, #3, #2, #13, #5, #10, #11, #8, #9, #6, #7, #12, #4, #15, #14, #1
+data: .word 0x10, 0x3, 0x2, 0xD, 0x5, 0xA, 0xB, 0x8, 0x9, 0x6, 0x7, 0xC, 0x4, 0xF, 0xE, 0x1
 .end
