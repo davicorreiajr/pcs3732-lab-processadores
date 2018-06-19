a6627614726442b7446c95e72502e9a0c3e37c59
Unknown <guilhermeadissy@gmail.com>
2018-06-19 16:45:29 -0300

    Creating columns loop

--- src/lab6/6.5.3-magic-squares.s
+++ src/lab6/6.5.3-magic-squares.s
@@ -1,41 +1,56 @@
-  .text
-  .globl main
+ .text
+ .globl main
 
 main: 
-  LDR r0, =dados 			  /* ponteiro para dados */
-  LDR r1, =4 				    /* N */
-  MUL r2, r1, r1		    /* r2 = N^2 */
-  ADD r2, r2, #1        /* r2 = N^2 + 1 */
-  MUL r2, r2, r1 			  /* r2 = N(N^2 + 1) */
-  MOV r2, r2, LSR #1		/* r2 = N(N^2 + 1)/2 */
+ LDR r0, =data            /* ponteiro para dados */
+ LDR r1, =4 				      /* N */
+ MUL r2, r1, r1		        /* r2 = N^2 */
+ ADD r2, r2, #1           /* r2 = N^2 + 1 */
+ MUL r2, r1, r2 			    /* r2 = N(N^2 + 1) */
+ MOV r2, r2, LSR #1		    /* r2 = N(N^2 + 1)/2 */
+ MOV r9, 0
 
 row:
-  MOV r3, r0
-  MOV r10, #0
+ MOV r3, r0
+ MOV r10, #0
 rowLoop:
-  CMP r10, r1
-  BGE column
-  LDMIA r3!, {r5-r8}
-  ADD r4, r5, r6
-  ADD r4, r4, r7
-  ADD r4, r4, r8
-  ADD r10, r10, #1
-  CMP r4, r2
-  BEQ rowLoop
-  B final
+ CMP r10, r1
+ BGE column
+ LDMIA r3!, {r5-r8}
+ ADD r4, r5, r6
+ ADD r4, r4, r7
+ ADD r4, r4, r8
+ ADD r10, r10, #1
+ CMP r4, r2
+ BEQ rowLoop
+ B final
 
 column:
-
-
+ MOV r3, r0
+ MOV r11, #0
 columnLoop:
-
+ CMP r11, r1
+ BGE diagonals
+ MOV r10, #0
+ MOV r6, #0
+columnSubLoop:
+ MUL r12, r10, r1
+ MOV r4, r11
+ ADD r4, r4, r12
+ LDR r5, [r4]
+ ADD r6, r6, r5
+ CMP r10, r1
+ BMI columnSubLoop
+ CMP r6, r2
+ BEQ columnLoop
+ B final
 
 diagonals:
-
+ MOV r9, #1
 
 
 final:
-  SWI 0x0
+ SWI 0x0
 
 
 
