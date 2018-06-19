fb83f08ff2eeb865215d54f5dba9cfe4f50093fe
Davi Cesar Correia Jr <davi@qulture.rocks>
2018-06-19 15:11:20 -0300

    finish first part of 6.5.1

--- src/lab6/6.5.1-transmiting-arguments.s
+++ src/lab6/6.5.1-transmiting-arguments.s
@@ -1,4 +1,14 @@
   .text
   .globl main
+  dados: .space 100
 
 main:
+by_register:
+  MOV r0, #2      @ b = 2
+  MOV r1, #3      @ c = 3
+  MOV r2, #5      @ d = 5
+  BL function_by_register
+  SWI 0x0
+
+function_by_register:
+  MLA r3, r0, r1, r2
