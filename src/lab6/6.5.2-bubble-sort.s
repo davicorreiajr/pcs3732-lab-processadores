2abc05af6260d453a28467c5dab113c417839907
Davi Cesar Correia Jr <davi@qulture.rocks>
2018-06-19 17:05:40 -0300

    fix bubble sort

--- src/lab6/6.5.2-bubble-sort.s
+++ src/lab6/6.5.2-bubble-sort.s
@@ -9,24 +9,25 @@ loop1:
   BLE final
   MOVS r1, r7                 @ r1 recebe ponteiro que ira varrer elementos (p*), iniciando na base do array
   ADDS r2, r1, r0             @ r2 recebe p* + n
+  SUB  r2, r2, #1
 loop2:
   CMP r1, r2
   BGE fimLoop2
   LDRB r3, [r1]
   LDRB r4, [r1, #1]!
   CMP r3, r4
-  BGT naoInverte
-  STRB r3, [r1, #-1]
-  STRB r4, [r1]
+  BLE naoInverte
+  STRB r4, [r1, #-1]
+  STRB r3, [r1]
 naoInverte:
-  ADDS r1, r1, #1
   B loop2
 fimLoop2:
   SUBS r0, r0, #1
   B loop1
 final:
+  MOV r0, r7
 
 
-dados: .byte 0x4, 0x2, 0x8, 0x21, 0x4
+dados: .byte 0x8, 0x2, 0x8, 0x21, 0x4, 0x16, 0x6, 0x1, 0xFF
 
 .end
