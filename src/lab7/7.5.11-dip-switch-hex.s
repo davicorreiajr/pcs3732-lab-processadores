cf011dbc2249ce9421962896dfeccf611c1236cb
Pedro Darvas <darvas.appsimples@gmail.com>
2018-07-03 16:58:26 -0300

    FINAL SWITCH HEX

--- src/lab7/7.5.11-dip-switch-hex.s
+++ src/lab7/7.5.11-dip-switch-hex.s
@@ -3,21 +3,45 @@
 
 main:
   LDR	r0, =0x3ff5000 	    @ r0 = IOPMOD
-  LDR r3, =0x3FF5008      @ r3 = IOPDATA
-  LDR	r1, =0xf0	          @ seta 1 nos bits [7:4] e 0 nos bits [3:0]
+  LDR	r1, =0xfe00	        @ seta 1 nos bits [16:10] e 0 nos bits [3:0]
   STR	r1, [r0]	          @ seta IOPMOD 
+  BL get_fourth_switch
+  MOVS r5, r7             @ valor inicial do switch. r5 é base de comparacao.
+  MOVS r2, #0             @ contador de vezes
+  BL print_in_hex
 loop:
-  LDR r2, [r3]            @ le os valores de switches do IOPDATA
-  MOVS r2, r2, LSL #4     @ shift 4 para esquerda
-  STR r2, [r3]            @ IOPDATA recebe numero a ser mostrado
+  BL get_fourth_switch
+  CMP r7, r5
+  BEQ loop
+  BL wait
+  CMP r7, r5
+  BEQ loop
+  ADD r2, r2, #1
+  MOV r5, r7
+  BL print_in_hex
   B loop
 end:
   MOVS s0, s0, #2
- 
+
+
+@ retorna em r7 o valor do 4o switch, 1 ou 0
+get_fourth_switch:
+  STMDB sp!, {r3}
+  LDR r3, =0x3FF5008
+  LDR r7, [r3]            @ le os valores de switches do IOPDATA
+  MOVS r7, r7, LSR #1
+  MOVCS r7, #1
+  MOVCC r7, #0
+  LDMIA sp!, {r3}
+  MOV pc, lr
+
+
 
  
-
-print_in_hex:             @ r2 deve ter o valor a imprimir
+@ Essa funcao recebe um numero 0 <= x < 16 e printa no display de 7 segmentos
+@ O numero x deve estar no  registrador r2
+@ Nao retorna nada
+print_in_hex:
   STMDB sp!, {r2, r3, r4, r5}
   LDR r3, =0x3FF5008      @ r3 = IOPDATA
   LDR r4, =display_values
@@ -25,6 +49,18 @@ print_in_hex:             @ r2 deve ter o valor a imprimir
   MOV r5, r5, LSL #10
   STR r5, [r3]            @ IOPDATA recebe numero a ser mostrado
   LDMIA sp!, {r2, r3, r4, r5}
+  MOV pc, lr
+
+  
+@ Espera um tempo
+wait:
+  STMDB sp!, {r4}
+  LDR r4, =0xfffff
+wait_loop:
+  SUBS r4, r4, #1
+  CMP r4, #0
+  BNE wait_loop
+  LDMIA sp!, {r4}
   MOV pc, lr
 
 
