208eab28e300a5e82a380eb55c8dd8ce3af12d54
Davi Cesar Correia Jr <davi@qulture.rocks>
2018-07-03 12:49:09 -0300

    7.5.1

---
+++ src/lab7/7.5.1-leds.s
@@ -0,0 +1,22 @@
+  .text
+  .globl main
+
+main:
+  LDR	r0, =0x3ff5000 	    @ r0 = IOPMOD
+  LDR r3, =0x3FF5008      @ r3 = IOPDATA
+  LDR	r1, =0xf0	          @ seta 1 nos bits [7:4]
+  STR	r1, [r0]	          @ seta IOPMOD como output
+  MOV r2, #0x0            @ r2 = 0
+seta:
+  STR r2, [r3]            @ IOPDATA recebe numero a ser mostrado
+  LDR r4, =0xffffffff
+  BL loop
+  ADDS r2, r2, #0x10      @ r2 = r2 + 0x10
+  CMP r2, #0xf0
+  BNE seta
+loop:
+  SUBS r4, r4, #1
+  CMP r4, #0
+  BNE loop
+fim_loop:
+  MOV pc, lr
