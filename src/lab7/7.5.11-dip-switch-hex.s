9958fc2d2ca1d0684d1a721939056851c2ff0e67
Pedro Darvas <darvas.appsimples@gmail.com>
2018-07-03 16:25:53 -0300

    fix display

--- src/lab7/7.5.11-dip-switch-hex.s
+++ src/lab7/7.5.11-dip-switch-hex.s
@@ -3,15 +3,19 @@
 
 main:
   LDR	r0, =0x3ff5000 	    @ r0 = IOPMOD
-  LDR	r1, =0xfe00	        @ seta 1 nos bits [16:10]
-  STR	r1, [r0]	          @ seta IOPMOD como output
-  LDR r6, =0x4000
-  MOV r7, #11
-  STR r7, [r6]
-seg_display:
-  LDR r2, [r6]            @ r2 = MEM[r6] (r2 = 0x4000)
-  BL print_in_hex
-  MOV r0, #0
+  LDR r3, =0x3FF5008      @ r3 = IOPDATA
+  LDR	r1, =0xf0	          @ seta 1 nos bits [7:4] e 0 nos bits [3:0]
+  STR	r1, [r0]	          @ seta IOPMOD 
+loop:
+  LDR r2, [r3]            @ le os valores de switches do IOPDATA
+  MOVS r2, r2, LSL #4     @ shift 4 para esquerda
+  STR r2, [r3]            @ IOPDATA recebe numero a ser mostrado
+  B loop
+end:
+  MOVS s0, s0, #2
+ 
+
+ 
 
 print_in_hex:             @ r2 deve ter o valor a imprimir
   STMDB sp!, {r2, r3, r4, r5}
@@ -23,5 +27,6 @@ print_in_hex:             @ r2 deve ter o valor a imprimir
   LDMIA sp!, {r2, r3, r4, r5}
   MOV pc, lr
 
-display_values:
+
+  display_values:
     .word    0x5f, 0x06, 0x3b, 0x2f, 0x66, 0x6d, 0x7d, 0x07, 0x7f, 0x6f, 0x77, 0x7c, 0x59, 0x3e, 0x79, 0x71
