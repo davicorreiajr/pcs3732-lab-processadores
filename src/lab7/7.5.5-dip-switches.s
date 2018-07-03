095404f941d1ecd19b786581d6346034b07c8c25
Pedro Darvas <darvas.appsimples@gmail.com>
2018-07-03 15:52:45 -0300

    DIP SWITCHES

---
+++ src/lab7/7.5.5-dip-switches.s
@@ -0,0 +1,15 @@
+  .text
+  .globl main
+
+main:
+  LDR	r0, =0x3ff5000 	    @ r0 = IOPMOD
+  LDR r3, =0x3FF5008      @ r3 = IOPDATA
+  LDR	r1, =0xf0	          @ seta 1 nos bits [7:4] e 0 nos bits [3:0]
+  STR	r1, [r0]	          @ seta IOPMOD 
+loop:
+  LDR r2, [r3]            @ le os valores de switches do IOPDATA
+  MOVS r2, r2, LSL #4     @ shift 4 para esquerda
+  STR r2, [r3]            @ IOPDATA recebe numero a ser mostrado
+  B loop
+end:
+  MOVS s0, s0, #2
