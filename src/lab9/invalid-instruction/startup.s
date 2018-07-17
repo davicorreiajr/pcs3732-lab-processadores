fdc62782eb66f722737a66ff0db64cfb4ce49381
Adissy <guilhermeadissy@gmail.com>
2018-07-17 17:15:51 -0300

    Creating the code for invalid instruction startup.s

--- src/lab9/invalid-instruction/startup.s
+++ src/lab9/invalid-instruction/startup.s
@@ -11,6 +11,17 @@ _Reset:
   B . /* FIQ */
  
 Reset_Handler:
+  @setando os valores de undefined
+  MRS r0, cpsr    @ salvando o modo corrente em R0
+  MSR cpsr_ctl, #0b11011011 @ alterando o modo para undefined - o SP eh automaticamente chaveado ao chavear o modo
+  LDR sp, =0x2000 @ a pilha de undefined eh setada 
+  MSR cpsr, r0 @ volta para o modo anterior
+  @setando os valores de supervisor
+  MRS r0, cpsr    @ salvando o modo corrente em R0
+  MSR cpsr_ctl, #0b11010011 @ alterando o modo para supervisor - o SP eh automaticamente chaveado ao chavear o modo
+  LDR sp, =0x1000 @ a pilha do supervisor eh setada 
+  MSR cpsr, r0 @ volta para o modo anterior
+
   LDR sp, =stack_top
   BL c_entry
   .word 0xffffffff
@@ -19,4 +30,5 @@ Reset_Handler:
 Undefined_Handler:
   LDR sp, =stack_top
   BL undefined
+  LDMFD sp!,{R0-R12,pc}^
   B .
