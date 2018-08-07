755fc5a3716129dea56099d8dbc4266fe37c25a5
davicorreiajr <davicorreiajr@gmail.com>
2018-08-07 12:59:08 -0300

    Add supervisorStack and irqStack to loader

--- src/lab11/main.s
+++ src/lab11/main.s
@@ -14,13 +14,13 @@ Reset:
 	@ setando o sp de IRQ
 	MRS r0, cpsr    					@ salvando o modo corrente em r0
 	MSR cpsr_ctl, #0b11010010 @ alterando o modo para IRQ - o SP eh automaticamente chaveado ao chavear o modo
-	LDR sp, =0x2000 					@ a pilha de IRQ eh setada 
+	LDR sp, =supervisorStack 	@ a pilha de IRQ eh setada 
 	MSR cpsr, r0 							@ volta para o modo anterior
 
 	@setando os sp do supervisor
 	MRS r0, cpsr    					@ salvando o modo corrente em R0
 	MSR cpsr_ctl, #0b11010011 @ alterando o modo para supervisor - o SP eh automaticamente chaveado ao chavear o modo
-	LDR sp, =0x1000 					@ a pilha do supervisor eh setada 
+	LDR sp, =irqStack 				@ a pilha do supervisor eh setada 
 	MSR cpsr, r0 							@ volta para o modo anterior
 
 	BL processSetup
